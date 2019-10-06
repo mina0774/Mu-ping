e6aa76f46404664675167d4f7823b8f50aa2f0d3
mina <mina0774@hanmail.net>
2019-10-06 16:20:17 +0900

    시작화면 -> 로그인 / 바로 사용하기, Cloud Vision 연동

--- app/src/main/java/com/google/sample/cloudvision/StartActivity.java
+++ app/src/main/java/com/google/sample/cloudvision/StartActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+//시작 화면 선택
 public class StartActivity extends AppCompatActivity {
 
     private Button gotoLogin;
