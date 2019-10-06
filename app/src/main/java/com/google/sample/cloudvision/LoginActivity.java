4ddfde83c9a690c3610514bdbdb8849e8bea640d
mina <mina0774@hanmail.net>
2019-10-06 15:10:30 +0900

    시작화면 -> 로그인 / 바로 사용하기, Cloud Vision 연동

---
+++ app/src/main/java/com/google/sample/cloudvision/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.google.sample.cloudvision;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnRegis;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnRegis = (Button) findViewById(R.id.btn_regis);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+        btnRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/google/sample/cloudvision/StartActivity.java
@@ -0,0 +1,39 @@
+package com.google.sample.cloudvision;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button gotoLogin;
+    private Button gotoMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        gotoLogin = (Button) findViewById(R.id.gotoLogin);
+        gotoMain= (Button) findViewById(R.id.gotoMain);
+
+        gotoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gotoMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
