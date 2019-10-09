e5d1b19fe1e9b76fd6188daf5df4e4a0ca2c9566
2nah <yinahee@naver.com>
2019-10-09 16:02:14 +0900

    login&register function

--- app/src/main/java/com/google/sample/cloudvision/LoginActivity.java
+++ app/src/main/java/com/google/sample/cloudvision/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,57 +15,97 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity {
 
-    private Button regis;
-    private Button login;
-    private EditText email_login;
-    private EditText pw_login;
+    // 비밀번호 정규식
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^[a-zA-Z0-9!@.#$%^&*?_~]{4,16}$");
+
+    // 파이어베이스 인증 객체 생성
     private FirebaseAuth firebaseAuth;
+
+    // 이메일과 비밀번호
+    private EditText editTextEmail2;
+    private EditText editTextPassword2;
+
+    private String email2 = "";
+    private String password2 = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        regis = (Button) findViewById(R.id.btn_regis);
-        login = (Button) findViewById(R.id.btn_login);
-        email_login = (EditText) findViewById(R.id.user_id);
-        pw_login = (EditText) findViewById(R.id.user_password);
+        // 파이어베이스 인증 객체 선언
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        firebaseAuth = firebaseAuth.getInstance();
-
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = email_login.getText().toString().trim();
-                String pw = pw_login.getText().toString().trim();
-
-                firebaseAuth.signInWithEmailAndPassword(email, pw)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                    startActivity(intent);
-                                } else {
-                                    Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-
-            }
-        });
-
-
-        regis.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
+        editTextEmail2 = findViewById(R.id.et_eamil2);
+        editTextPassword2 = findViewById(R.id.et_password2);
 
     }
+
+    public void signIn(View view) {
+        email2 = editTextEmail2.getText().toString();
+        password2 = editTextPassword2.getText().toString();
+
+        if(isValidEmail() && isValidPasswd()) {
+            loginUser(email2, password2);
+        }
+    }
+
+    // 이메일 유효성 검사
+    private boolean isValidEmail() {
+        if (email2.isEmpty()) {
+            // 이메일 공백
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email2).matches()) {
+            // 이메일 형식 불일치
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // 비밀번호 유효성 검사
+    private boolean isValidPasswd() {
+        if (password2.isEmpty()) {
+            // 비밀번호 공백
+            return false;
+        } else if (!PASSWORD_PATTERN.matcher(password2).matches()) {
+            // 비밀번호 형식 불일치
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // 로그인
+    private void loginUser(String email2, String password2)
+    {
+        firebaseAuth.signInWithEmailAndPassword(email2, password2)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 로그인 성공
+                            Toast.makeText(LoginActivity.this, R.string.success_login, Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            
+                        } else {
+                            // 로그인 실패
+                            Toast.makeText(LoginActivity.this, R.string.failed_login, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    public void register(View view) {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
--- app/src/main/java/com/google/sample/cloudvision/RegisterActivity.java
+++ app/src/main/java/com/google/sample/cloudvision/RegisterActivity.java
@@ -1,11 +1,11 @@
 package com.google.sample.cloudvision;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,45 +14,91 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText et_id;
-    private EditText et_pw;
-    private Button btn_done;
+    // 비밀번호 정규식
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^[a-zA-Z0-9!@.#$%^&*?_~]{4,16}$");
+
+    // 파이어베이스 인증 객체 생성
     private FirebaseAuth firebaseAuth;
+
+    // 이메일과 비밀번호
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private String email = "";
+    private String password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        et_id = (EditText) findViewById(R.id.et_id);
-        et_pw = (EditText) findViewById(R.id.et_pw);
-        btn_done = (Button) findViewById(R.id.btn_done);
-
+        // 파이어베이스 인증 객체 선언
         firebaseAuth = FirebaseAuth.getInstance();
 
-        btn_done.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = et_id.getText().toString().trim();
-                String pw = et_pw.getText().toString().trim();
+        editTextEmail = findViewById(R.id.et_eamil);
+        editTextPassword = findViewById(R.id.et_password);
+    }
 
-                firebaseAuth.createUserWithEmailAndPassword(email, pw).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+    public void singUp(View view) {
+        email = editTextEmail.getText().toString();
+        password = editTextPassword.getText().toString();
+
+        if(isValidEmail() && isValidPasswd()) {
+            createUser(email, password);
+        }
+    }
+
+
+    // 이메일 유효성 검사
+    private boolean isValidEmail() {
+        if (email.isEmpty()) {
+            // 이메일 공백
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            // 이메일 형식 불일치
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // 비밀번호 유효성 검사
+    private boolean isValidPasswd() {
+        if (password.isEmpty()) {
+            // 비밀번호 공백
+            return false;
+        } else if (!PASSWORD_PATTERN.matcher(password).matches()) {
+            // 비밀번호 형식 불일치
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // 회원가입
+    private void createUser(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-
                         if (task.isSuccessful()) {
-                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                            startActivity(intent);
-                            finish();
+                            // 회원가입 성공
+                            Toast.makeText(RegisterActivity.this, R.string.success_signup, Toast.LENGTH_SHORT).show();
                         } else {
-                            Toast.makeText(RegisterActivity.this, "회원가입 에러", Toast.LENGTH_SHORT).show();
-                            return;
+                            // 회원가입 실패
+                            Toast.makeText(RegisterActivity.this, R.string.failed_signup, Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
-});
+
+    public void back(View view) {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
+
 }
