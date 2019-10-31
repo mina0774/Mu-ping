87d3380f4d67be47e2dd4179e8227977576f38c3
2nah <51187226+2nah@users.noreply.github.com>
2019-11-01 01:18:18 +0900

    Update User.java

--- app/src/main/java/com/google/sample/cloudvision/User.java
+++ app/src/main/java/com/google/sample/cloudvision/User.java
@@ -2,9 +2,15 @@ package com.google.sample.cloudvision;
 
 public class User {
     private String email;
+    private String genre;
 
-    public User(String email) {
+    public User(String email, String genre) {
         this.email = email;
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
     }
 
     public String getEmail() {
